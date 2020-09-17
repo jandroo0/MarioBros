8515dd01e30c439c118789136e17931dd97750b9
jandroo0 <fortenacer@gmail.com>
2020-09-17 18:57:17 -0400

    new marioBros game

---
+++ MarioBros/core/src/com/garcia/mariobros/MarioBros.java
@@ -0,0 +1,26 @@
+package com.garcia.mariobros;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.garcia.mariobros.Screens.GameScreen;
+
+public class MarioBros extends Game {
+	public SpriteBatch batch;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new GameScreen(this));
+	}
+
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
---
+++ MarioBros/core/src/com/garcia/mariobros/Screens/GameScreen.java
@@ -0,0 +1,60 @@
+package com.garcia.mariobros.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.garcia.mariobros.MarioBros;
+
+public class GameScreen implements Screen {
+
+    private MarioBros game;
+    private OrthographicCamera camera;
+    private Viewport gamePort;
+
+    public GameScreen(MarioBros game) {
+        this.game = game;
+        camera = new OrthographicCamera();
+        gamePort = new StretchViewport(800, 480, camera);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(camera.combined);
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
