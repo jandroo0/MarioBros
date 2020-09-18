5a5d5116ab7fc6d50efb53e86679901f46b444eb
jandroo0 <fortenacer@gmail.com>
2020-09-18 10:29:19 -0400

    hud

--- MarioBros/core/src/com/garcia/mariobros/Screens/GameScreen.java
+++ MarioBros/core/src/com/garcia/mariobros/Screens/GameScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.garcia.mariobros.MarioBros;
+import com.garcia.mariobros.Scenes.Hud;
 
 public class GameScreen implements Screen {
 
@@ -14,10 +15,14 @@ public class GameScreen implements Screen {
     private OrthographicCamera camera;
     private Viewport gamePort;
 
+    private Hud hud;
+
     public GameScreen(MarioBros game) {
         this.game = game;
         camera = new OrthographicCamera();
-        gamePort = new StretchViewport(800, 480, camera);
+        gamePort = new StretchViewport(MarioBros.V_WIDTH, MarioBros.v_HEIGHT, camera);
+
+        hud = new Hud(game.batch);
     }
 
     @Override
@@ -27,9 +32,12 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClearColor(0.5f,0.5f,0.7f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.setProjectionMatrix(camera.combined);
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+
+
 
     }
 
