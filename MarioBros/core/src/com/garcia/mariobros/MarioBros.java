f52295955f32bdfea8b106883c08b0d59267d24d
jandroo0 <fortenacer@gmail.com>
2020-09-18 12:31:22 -0400

    tilemap, box2d, world

--- MarioBros/core/src/com/garcia/mariobros/MarioBros.java
+++ MarioBros/core/src/com/garcia/mariobros/MarioBros.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.garcia.mariobros.Screens.GameScreen;
 
 public class MarioBros extends Game { // from windows
-	public static final int V_WIDTH = 400;
+	public static final int V_WIDTH = 416;
 	public static final int v_HEIGHT = 208;
 	public SpriteBatch batch;
 
--- MarioBros/core/src/com/garcia/mariobros/Screens/GameScreen.java
+++ MarioBros/core/src/com/garcia/mariobros/Screens/GameScreen.java
@@ -4,10 +4,25 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.garcia.mariobros.MarioBros;
 import com.garcia.mariobros.Scenes.Hud;
+
+import javax.xml.bind.ValidationException;
 
 public class GameScreen implements Screen {
 
@@ -17,12 +32,105 @@ public class GameScreen implements Screen {
 
     private Hud hud;
 
+    // tiled map
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+    // box2d vars
+    private World world;
+    private Box2DDebugRenderer b2dr;
+
     public GameScreen(MarioBros game) {
         this.game = game;
         camera = new OrthographicCamera();
         gamePort = new StretchViewport(MarioBros.V_WIDTH, MarioBros.v_HEIGHT, camera);
 
         hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("tilemap/tilemap.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        camera.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0); // center camera
+
+        world = new World(new Vector2(0,0), true);
+        b2dr = new Box2DDebugRenderer();
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+        Body body;
+
+        // create ground fixtures
+        for(MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        // create pipe fixtures
+        for(MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        // create coin fixtures
+        for(MapObject object : map.getLayers().get(4).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+
+        // create brick fixtures
+        for(MapObject object : map.getLayers().get(5).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
+
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+    }
+
+    // player input
+    public void handleInput(float deltaTime) {
+        if(Gdx.input.isTouched()) {
+            camera.position.x += 100 * deltaTime;
+        }
+    }
+
+    public void update(float deltaTime) {
+        handleInput(deltaTime);
+
+        camera.update(); // update camera
+        renderer.setView(camera);
     }
 
     @Override
@@ -31,14 +139,18 @@ public class GameScreen implements Screen {
     }
 
     @Override
-    public void render(float delta) {
+    public void render(float deltaTime) {
+        update(deltaTime);
+
         Gdx.gl.glClearColor(0.5f,0.5f,0.7f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
-        hud.stage.draw();
 
+        renderer.render(); // render game map
 
+        b2dr.render(world, camera.combined); //render box2DDebugLines
 
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined); // set batch to what camera sees
+        hud.stage.draw(); // draw hud
     }
 
     @Override
